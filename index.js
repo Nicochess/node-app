304e0e381133a97654f7de6ee651c8d4a891826c
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-31 23:40:17 -0300

    feat: post setting up

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Testing express')
 })
 
+app.post('/', (req, res) => {
+    res.send('Post response method')
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
