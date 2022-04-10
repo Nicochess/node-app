0f335f81714c48e81af1ebe818d46fdca6da6b43
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-04-09 21:02:20 -0300

    feat: post method created

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const uuid = require("uuid");
 const router = express.Router();
 const users = require("../../Users");
 
@@ -8,17 +8,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const found = users.some((user) => user.id === Number(req.params.id));
+  const found = users.some((user) => user.id === parseInt(req.params.id));
 
   if (found) {
-    res.json(users.filter((user) => user.id === Number(req.params.id)));
+    res.json(users.filter((user) => user.id === parseInt(req.params.id)));
   } else {
     res.status(400).json({ msg: `No user with id ${req.params.id}` });
   }
 });
 
 router.post("/", (req, res) => {
-  res.send(res.body)
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  }
+
+  if(!newMember.name || !newMember.email){
+    return res.status(400).json({ msg: 'Please include a name and email' })
+  }
+
+  users.push(newMember);
+  res.json(users)
 });
+
+router.put("/:id", (req, res) => {
+  const found = users.some(user => user.id === parseInt(req.params.id))
+})
 
 module.exports = router;
